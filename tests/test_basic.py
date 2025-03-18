81ce709ba88476cf463385d9ae48476e46553b8e
Hristina Ivanova <hristina.ivanova@tum.de>
2025-03-18 21:25:07 +0100

    trigger pipeline

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -21,4 +21,4 @@ class TemplateClassTest(unittest.TestCase):
 
     def test_number_of_lanelets(self):
         self.assertEqual(self.object.return_number_of_lanelets(), 12)
-        print("hello world")
+        print("hello world!!")
