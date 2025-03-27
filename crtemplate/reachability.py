94cb244e4d3810f454bdc1d8881cb88ea88220e4
hrisi <h.v.ivanova@abv.bg>
2025-03-27 16:07:09 +0100

    added functions for computing drivable area at each time step

--- crtemplate/reachability.py
+++ crtemplate/reachability.py
@@ -1,6 +1,5 @@
-import pathlib
-
 import commonroad_reach.utility.logger as util_logger
+import numpy as np
 from commonroad_reach.data_structure.configuration_builder import ConfigurationBuilder
 from commonroad_reach.data_structure.reach.reach_interface import ReachableSetInterface
 from commonroad_reach.utility import visualization as util_visual
@@ -9,7 +8,7 @@ from commonroad_reach.utility import visualization as util_visual
 # Loads a scenario from an xml file. params: scenario (str) - name of the scenario
 # Computes reachable set of ego vehicle and outputs it as a gif
 def load_scenario_and_compute_reachability(scenario):
-    # === Build configuration
+    # Build configuration
     config = ConfigurationBuilder(path_root="../..").build_configuration(scenario)
     config.update()
 
@@ -17,14 +16,42 @@ def load_scenario_and_compute_reachability(scenario):
     logger = util_logger.initialize_logger(config)
     config.print_configuration_summary()
 
-    # === Compute reachable sets
+    # Compute reachable sets
     reach_interface = ReachableSetInterface(config)
     reach_interface.compute_reachable_sets()
 
-    # === Plot computation results
+    # Compute the drivable area at each step
+    drivable_area = compute_full_drivable_area(reach_interface)
+    print("The drivable area at the start is:", drivable_area[0])
+
+    # Plot computation results
     util_visual.plot_scenario_with_reachable_sets(reach_interface, figsize=(7, 7))
 
     return reach_interface
+
+
+def compute_drivable_slice_area(drivable_slice):
+    area = 0
+
+    for region in drivable_slice:
+        width = region.p_lon_max - region.p_lon_min
+        height = region.p_lat_max - region.p_lat_min
+        area += width * height
+
+    return area
+
+
+def compute_full_drivable_area(reach_interface):
+    drivable_area = np.array(
+        [-1.0 for i in range(reach_interface.step_start, reach_interface.step_end + 1)]
+    )
+
+    for time_step in range(reach_interface.step_start, reach_interface.step_end + 1):
+        drivable_slice = reach_interface.drivable_area_at_step(time_step)
+        area = compute_drivable_slice_area(drivable_slice)
+        drivable_area[time_step] = area
+
+    return drivable_area
 
 
 scenario = "ZAM_Tjunction-1_307_T-1"
