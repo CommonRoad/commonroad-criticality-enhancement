91a85fbda15ea4a2be554555db8feee0a2ab822f
hrisi <h.v.ivanova@abv.bg>
2025-03-29 10:13:53 +0100

    changed folder name

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,7 +1,9 @@
 import os
 import sys
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "crtemplate")))
+sys.path.append(
+    os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "enhance_criticality"))
+)
 import pathlib
 import unittest
 
--- tutorials/usage.py
+++ tutorials/usage.py
@@ -2,7 +2,9 @@ import os
 import sys
 
 # Add the parent directory (my_project) to the system path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "crtemplate")))
+sys.path.append(
+    os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "enhance_criticality"))
+)
 
 import pathlib
 
