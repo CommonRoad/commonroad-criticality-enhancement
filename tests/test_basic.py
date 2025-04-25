959c17f6db7170eff13086bf4b0dc4553a992e4d
hrisi <h.v.ivanova@abv.bg>
2025-04-25 12:26:02 +0200

    fix pipelines

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -3,12 +3,11 @@ import sys
 import unittest
 
 import numpy as np
-import ../enhance_criticality
 
 sys.path.append(
     os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "enhance_criticality"))
 )
-from enhance_criticality.reachability import (
+from reachability import (
     compute_drivable_slice_area,
     compute_full_drivable_area,
     differentiate_reachable_set_wrt_position,
