edc2f3b0daabfa92e41eb4b19e8dc1e0d7533aa1
Hristina Ivanova <hristina.ivanova@tum.de>
2025-07-12 13:24:22 +0200

    add test

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,17 +1,22 @@
 import numpy as np
-import pytest
 from commonroad.scenario.lanelet import Lanelet
 from commonroad.scenario.scenario import Scenario, ScenarioID
 
 
-@pytest.fixture
-def scenario() -> Scenario:
-    lanelet = Lanelet(np.array([[0, 2], [1, 2]]), np.array([[0, 1], [1, 1]]), np.array([[0, 0], [1, 0]]), 51)
+def test_scenario_has_lanelet():
+    left_bound = np.array([[0, 2], [1, 2]])
+    center_line = np.array([[0, 1], [1, 1]])
+    right_bound = np.array([[0, 0], [1, 0]])
+
+    # Create a Lanelet object with ID 51
+    lanelet = Lanelet(left_bound, center_line, right_bound, 51)
+
+    # Create a new Scenario object
     scenario = Scenario(0.2, ScenarioID())
+
+    # Add the lanelet to the scenario
     scenario.add_objects([lanelet])
-    return scenario
 
-
-def test_scenario_has_lanelet(scenario):
+    # Assertions
     assert len(scenario.lanelet_network.lanelets) == 1
     assert scenario.lanelet_network.find_lanelet_by_id(51) is not None
